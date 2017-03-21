bebda4215dc768c95b4e5241b4f5ef5f8d3c2e99
MABIAO <mabiao@zillionfortune.com>
2017-03-21 17:39:55 +0800

    dev

--- mq-service/src/main/java/com/mxz/mq/thread/ThreadTest.java
+++ mq-service/src/main/java/com/mxz/mq/thread/ThreadTest.java
@@ -8,6 +8,8 @@ public class ThreadTest {
     public static void main(String args[]){
         MyThread thread1 = new MyThread();
         MyThread thread2 = new MyThread();
+        thread2.setPriority(10);
+        thread1.setPriority(10);
         thread1.start();
         thread2.start();
     }
