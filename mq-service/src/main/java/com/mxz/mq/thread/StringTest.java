73c1a161c04ba3a84639764332332c68b02796da
MABIAO <mabiao@zillionfortune.com>
2017-03-28 15:33:56 +0800

    dev

---
+++ mq-service/src/main/java/com/mxz/mq/thread/StringTest.java
@@ -0,0 +1,23 @@
+package com.mxz.mq.thread;
+
+/**
+ * Created by MABIAO on 2017/3/23 0023.
+ */
+public class StringTest {
+
+    static String ss;
+
+    public static void main(String args[]){
+//        String s1 = "abc";
+//        String s2 = "abc";
+//        String s3 = new String("abc");
+//
+//        String s = Boolean.TRUE.toString();
+//
+//        System.out.println("s1 == s2:" + (s1 == s2));
+//        System.out.println(s1.equals(s3));
+//        System.out.println(s);
+
+        System.out.println(StringTest.ss);
+    }
+}
--- mq-service/src/main/java/com/mxz/mq/thread/ThreadTest.java
+++ mq-service/src/main/java/com/mxz/mq/thread/ThreadTest.java
@@ -6,11 +6,22 @@ package com.mxz.mq.thread;
 public class ThreadTest {
 
     public static void main(String args[]){
-        MyThread thread1 = new MyThread();
-        MyThread thread2 = new MyThread();
-        thread2.setPriority(10);
-        thread1.setPriority(10);
-        thread1.start();
-        thread2.start();
+        int t = 6/0;
+        try {
+            for (int i = 0; i < 100000000; i++){
+                MyThread thread = new MyThread();
+                thread.start();
+            }
+            MyThread thread1 = new MyThread();
+            MyThread thread2 = new MyThread();
+            thread2.setPriority(10);
+            thread1.setPriority(10);
+            Thread.sleep(36000L);
+            thread1.start();
+            thread2.start();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
